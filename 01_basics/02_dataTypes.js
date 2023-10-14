887d2aac8bc4abb0ac5d1ce991bf53eb25b4c932
BhawaniK <bk.sambhaw@gmail.com>
2023-10-15 00:23:19 +0530

    feature_added DataType.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Treats all code as newer version.
+
+// alert(3+3) //! In NodeJS,  we cannot use alert directly.
+
+// Datatype Types
+//- String
+let name = "Abc";
+//- Number, limit raises till
+let age = 23;
+//- Boolean, true/False
+let isLoggedIn = false;
+//- null, a standalone value
+let state = null;
+//- undefined, value has not been assigned yet.
+let lang;
+//-BigInt, for long numbers
+let stocks = 884169965635298656356895681778175n;
+//- Symbol, unique
+
+//-Object
+// console.log(typeof name); //? string
+// console.log(typeof age); //? Number
+// console.log(typeof null);//? Object
+// console.log(typeof undefined);//? undefined
+
